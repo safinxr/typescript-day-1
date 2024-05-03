7618df5687362c7a1d11f7bbf6adc9d81f3a2f93
safinxr <safinxr@gmail.com>
2024-05-03 10:55:25 +0600

    genetic type done

--- module2/test.0.1.ts
+++ module2/test.0.1.ts
@@ -1,62 +1,74 @@
 {
-    // 
+  //
+
+  // type assertion
+
+  let alu: any;
+
+  alu = "hello bro";
+
+  alu as string;
+
+  const add = (a: number): number => a + 5;
+
+  const bb = add(5) as number;
+
+  // interface
+
+  type User1 = {
+    name: string;
+    phone: number;
+  };
+
+  type User3 = User1 & { gender?: string };
+  interface User2 {
+    name: string;
+    phone: number;
+  }
+
+  interface User4 extends User2 {
+    gender: string;
+  }
+
+  const user1: User1 = {
+    name: "Safin",
+    phone: 3343434,
+  };
+
+  const user2: User2 = {
+    name: "Safin",
+    phone: 3343434,
+  };
+  const user3: User3 = {
+    name: "Safin",
+    phone: 3343434,
+  };
+  const user4: User4 = {
+    name: "Safin",
+    phone: 3343434,
+    gender: "male",
+  };
+
+  // generic type
+
+  type GenericArray<T> = Array<T>;
 
 
-// type assertion
+  const array : GenericArray<number> = [34, 4, 4, 4]
 
-let alu: any;
-
-alu = 'hello bro';
-
-(alu as string)
-
-
-
-const add = (a:number) :number => a + 5;
-
-
-const bb = add(5) as number
-
-
-
-// interface
-
-type User1 = {
+  const student: GenericArray<{
     name:string;
-    phone:number;
-}
-
-
-type User3 = User1 & { gender?: string };
-interface User2 {
-  name: string;
-  phone: number;
-}
-
-interface User4 extends User2 {
-    gender:string
-}
-
-const user1 :User1 ={
-    name:"Safin",
-    phone:3343434,
-}
-
-const user2 :User2 ={
-    name:"Safin",
-    phone:3343434,
-}
-const user3 :User3 ={
-    name:"Safin",
-    phone:3343434,
-
-}
-const user4 :User4 ={
-    name:"Safin",
-    phone:3343434,
-    gender:'male'
-
-}
+    roll:number;
+  }> =[
+    {
+        name:"alu",
+        roll: 4
+    },
+    {
+        name: 'jaja',
+        roll: 343
+    },
+  ]
 
 
 
@@ -70,12 +82,5 @@ const user4 :User4 ={
 
 
 
-
-
-
-
-
-
-    
-    // 
+  //
 }
