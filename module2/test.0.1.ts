d9bf4d6087dcff1480edd48cac257977b29d9c6b
safinxr <safinxr@gmail.com>
2024-05-03 10:16:14 +0600

    what is assertion

---
+++ module2/test.0.1.ts
@@ -0,0 +1,46 @@
+{
+    // 
+
+
+// type assertion
+
+let alu: any;
+
+alu = 'hello bro';
+
+(alu as string)
+
+
+
+const add = (a:number) :number => a + 5;
+
+
+const bb = add(5) as number
+
+
+
+// interface
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    // 
+}
