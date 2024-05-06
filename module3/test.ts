712dae85dab070b179b46c9b75150af9785f301f
safinxr <safinxr@gmail.com>
2024-05-06 15:46:21 +0600

    Inheritance

--- module3/test.ts
+++ module3/test.ts
@@ -1,25 +1,50 @@
 {
   // OOP OR Object Oriented Programming
 
-  class Animal {
-    sound: string;
+//   class Animal {
+//     sound: string;
 
-    // Parameter properties == set public before parameter
+//     // Parameter properties == set public before parameter
 
-    constructor(public name: string, public gender: string, sound: string) {
-      this.sound = sound;
-    }
+//     constructor(public name: string, public gender: string, sound: string) {
+//       this.sound = sound;
+//     }
 
-    makeSound(time: number) {
-      console.log(`${this.name} makes sound ${time} times`);
-    }
-  }
+//     makeSound(time: number) {
+//       console.log(`${this.name} makes sound ${time} times`);
+//     }
+//   }
 
-  const dog = new Animal("pochi", "male", "vaw vaw");
+//   const dog = new Animal("pochi", "male", "vaw vaw");
 //   dog.makeSound(5);
 
 
+ class Animal {
 
+   constructor(public name: string, public gender: string, public sound: string) {
+   }
+
+   makeSound(time: number) {
+     console.log(`${this.name} makes sound ${time} times`);
+   }
+ }
+
+ const dog = new Animal("pochi", "male", "vaw vaw");
+
+
+ class Beast extends Animal {
+   constructor(name: string, gender: string, sound: string, color: string) {
+     super(name, gender, sound);
+   }
+
+   newBeast(time: number) {
+     console.log(`${this.name} makes sound ${time} times`);
+   }
+ }
+
+ const darkDog = new Beast("Dark pochi", "male", "vaw vaw", "black");
+
+ darkDog.makeSound(100)
 
 
 
